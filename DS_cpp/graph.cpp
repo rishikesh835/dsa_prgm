263723533e95196cf662fb6f95ce497c4b704eaf
rishikesh835 <rishikeshmishra835@gmail.com>
2025-07-16 12:16:41 +0530

    trie added

--- DS_cpp/graph.cpp
+++ DS_cpp/graph.cpp
@@ -1,82 +1,99 @@
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <list>
 #include <queue>
-#include<limits.h>
-#include<set>
+#include <limits.h>
+#include <set>
 using namespace std;
-class Graph{
+class Graph
+{
     int V;
-    list <int> *l;
-    public:
-    Graph(int v) {
+    list<int> *l;
+
+public:
+    Graph(int v)
+    {
         V = v;
         l = new list<int>[v];
     }
-    void addEdge(int i,int j,bool undir = true){
+    void addEdge(int i, int j, bool undir = true)
+    {
         l[i].push_back(j);
-        if(undir){
+        if (undir)
+        {
             l[j].push_back(i);
         }
     }
-    void printAdjList(){
-        for(int i = 0;i<V;i++) {
-            cout<<i<<"-->";
-            for(auto node:l[i]){
-                cout<<node<<",";
+    void printAdjList()
+    {
+        for (int i = 0; i < V; i++)
+        {
+            cout << i << "-->";
+            for (auto node : l[i])
+            {
+                cout << node << ",";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
-    void bfs(int source) {
+    void bfs(int source)
+    {
         queue<int> q;
         bool *visited = new bool[V]{0};
         q.push(source);
         visited[source] = true;
 
-        while(!q.empty()){
+        while (!q.empty())
+        {
             int f = q.front();
-            cout<< f<<",";
+            cout << f << ",";
             q.pop();
 
-            for(auto nbr: l[f]){
-                if(!visited[nbr]) {
+            for (auto nbr : l[f])
+            {
+                if (!visited[nbr])
+                {
                     q.push(nbr);
                     visited[nbr] = true;
                 }
             }
         }
     }
-    void dfsHelper(int node,bool *visited){
+    void dfsHelper(int node, bool *visited)
+    {
         visited[node] = true;
-        cout<<node<<",";
-        //make a dfs call on it's unvisited neighbor
-        for(int nbr : l[node]){
-            if(!visited[nbr]){
-                dfsHelper(nbr,visited);
+        cout << node << ",";
+        // make a dfs call on it's unvisited neighbor
+        for (int nbr : l[node])
+        {
+            if (!visited[nbr])
+            {
+                dfsHelper(nbr, visited);
             }
         }
         return;
     }
-    void dfs(int source) {
+    void dfs(int source)
+    {
         bool *visited = new bool[V]{0};
-        dfsHelper(source,visited);
+        dfsHelper(source, visited);
     }
-    //will need to decalre a weighted graph
-    
+    // will need to decalre a weighted graph
 };
-int main(){
+int main()
+{
     Graph g(6);
-    g.addEdge(0,1);
-    g.addEdge(0,4);
-    g.addEdge(2,1);
-    g.addEdge(3,4);
-    g.addEdge(4,5);
-    g.addEdge(2,3);
-    g.addEdge(3,5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 4);
+    g.addEdge(2, 1);
+    g.addEdge(3, 4);
+    g.addEdge(4, 5);
+    g.addEdge(2, 3);
+    g.addEdge(3, 5);
     g.printAdjList();
-    cout<<"bfs\n";
+    cout << "bfs\n";
     g.bfs(1);
-    cout<<"\n"<<"dfs"<<"\n";
+    cout << "\n"
+         << "dfs" << "\n";
     g.dfs(1);
     return 0;
 }
--- DS_cpp/graph_dijkstra.cpp
+++ DS_cpp/graph_dijkstra.cpp
@@ -1,29 +1,34 @@
-#include<iostream>
-#include<list>
-#include<set>
-#include<vector>
-#include<limits.h>
+#include <iostream>
+#include <list>
+#include <set>
+#include <vector>
+#include <limits.h>
 using namespace std;
 
-class Graph{
+class Graph
+{
     int V;
-    list<pair<int,int>> *l;
-    public:
-    Graph(int v){
+    list<pair<int, int>> *l;
+
+public:
+    Graph(int v)
+    {
         V = v;
-        l = new list<pair<int,int>>[V];
+        l = new list<pair<int, int>>[V];
     }
-    void addEdge(int u,int v,int wt,bool undir = true){
-        l[u].push_back({wt,v});
-        if(undir){
-            l[v].push_back({wt,u});
+    void addEdge(int u, int v, int wt, bool undir = true)
+    {
+        l[u].push_back({wt, v});
+        if (undir)
+        {
+            l[v].push_back({wt, u});
         }
     }
     int dijkstra(int src, int dest)
     {
         // Data structures
         vector<int> dist(V, INT_MAX);
-        //pair<distance,source>
+        // pair<distance,source>
         set<pair<int, int>> s;
 
         // l,Init
@@ -45,34 +50,35 @@ class Graph{
                 {
                     // remove if nbr already exist in the set
 
-                    auto f = s.find({dist[nbr],nbr});
-                    if(f != s.end()){
+                    auto f = s.find({dist[nbr], nbr});
+                    if (f != s.end())
+                    {
                         s.erase(f);
                     }
-                    // insert the updated value with the new distance 
+                    // insert the updated value with the new distance
                     dist[nbr] = distTillNow + currentEdge;
                     s.insert({dist[nbr], nbr});
                 }
             }
         }
-        //single source shortest dist to all other nodes
+        // single source shortest dist to all other nodes
         for (int i = 0; i < V; i++)
         {
-            cout<<"Node i "<<i<<" Dist "<<dist[i]<<endl;
+            cout << "Node i " << i << " Dist " << dist[i] << endl;
         }
         return dist[dest];
     }
-   
 };
-int main(){
+int main()
+{
     Graph g(5);
-    g.addEdge(0,1,1);    
-    g.addEdge(1,2,2);    
-    g.addEdge(0,2,4);    
-    g.addEdge(0,3,7);    
-    g.addEdge(3,2,2);    
-    g.addEdge(3,4,3); 
-    
-    cout<<g.dijkstra(0,4);
+    g.addEdge(0, 1, 1);
+    g.addEdge(1, 2, 2);
+    g.addEdge(0, 2, 4);
+    g.addEdge(0, 3, 7);
+    g.addEdge(3, 2, 2);
+    g.addEdge(3, 4, 3);
+
+    cout << g.dijkstra(0, 4);
     return 0;
 }
